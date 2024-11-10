cfefbc28cb33cd364bedac2029e03cf20307a9e8
nabab <thomas.nabet@gmail.com>
2024-11-11 00:20:16 +0100

    Fix in comments for controller and model (wrong order)

--- src/mvc/public/events.php
+++ src/mvc/public/events.php
@@ -2,7 +2,7 @@
 
 use bbn\X;
 use bbn\Str;
-/** @var $ctrl \bbn\Mvc\Controller */
+/** @var bbn\Mvc\Controller $ctrl */
 
 $ctrl->setIcon('nf nf-oct-calendar')
   ->setUrl($ctrl->pluginUrl('appui-event').'/events')
